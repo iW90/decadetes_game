1bbd13a3792f929115fd104b5d10c146cf5fa7d6
Everton <egeraldo@student.42sp.org.br>
2025-09-20 19:42:13 -0300

    feat: adiciona lógica de ataque e transição de estado para o inimigo
    - Inicializa a variável `player` no método `_ready` do inimigo.
    - Adiciona a transição de estado para "auto-move" quando o jogador sai da área de ataque.
    - Atualiza a lógica de entrada na área de ataque para mudar o estado para "attack".
    - Remove código não utilizado do script de ataque.

--- scenes/state-machine/scripts/attack.gd
+++ scenes/state-machine/scripts/attack.gd
@@ -13,24 +13,4 @@ func process_input(_event: InputEvent):
 	return null
 
 func process_physics(_delta: float):
-	#change_face(parent.global_position)
 	return null
-
-func change_face(direction: Vector2) -> void:
-	if not "face" in parent:
-		return
-
-	var dir: String = ""
-
-	if abs(direction.x) > abs(direction.y):
-		if direction.x > 0:
-			dir = "right"
-		else:
-			dir = "left"
-	else:
-		if direction.y > 0:
-			dir = "down"
-		else:
-			dir = "up"
-	parent.face = dir
-	update_animation()
