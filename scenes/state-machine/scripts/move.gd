9edf1012b91904cb33b3963ec69ca96f508613f7
Everton <egeraldo@student.42sp.org.br>
2025-09-20 21:26:35 -0300

    arrumando a movimentação

--- scenes/state-machine/scripts/move.gd
+++ scenes/state-machine/scripts/move.gd
@@ -9,7 +9,7 @@ extends State
 
 func enter() -> void:
 	if !Input.is_action_pressed("backwards"):
-		change_face(Constants.get_move_vector())
+		update_face_direction(Constants.get_move_vector())
 	super()
 
 func process_input(_event: InputEvent):
@@ -19,7 +19,6 @@ func process_input(_event: InputEvent):
 
 func process_physics(_delta: float):
 	var dir = Constants.get_move_vector()
-	change_face(dir)
 	if dir == Vector2.ZERO:
 		parent.velocity = Vector2.ZERO
 
@@ -31,21 +30,8 @@ func process_physics(_delta: float):
 				return idle_state
 		parent.move_and_slide()
 		return null
+
 	parent.velocity = dir * move_speed
-	change_face(parent.velocity)
+	update_face_direction(dir)
 	parent.move_and_slide()
 	return null
-
-func change_face(dir: Vector2) -> void:
-	if "face" not in parent: return
-	if abs(dir.x) > abs(dir.y):
-		if dir.x > 0:
-			parent.face = "right"
-		else:
-			parent.face = "left"
-	else:
-		if dir.y > 0:
-			parent.face = "down"
-		else:
-			parent.face = "up"
-	update_animation()
