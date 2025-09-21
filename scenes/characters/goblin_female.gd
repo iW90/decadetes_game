da7e2d2e1d99635d12d8870a047571a3b48426d3
Marcos <marcos.chiu@gmail.com>
2025-09-20 23:18:15 -0300

    health bar

--- scenes/characters/goblin_female.gd
+++ scenes/characters/goblin_female.gd
@@ -3,12 +3,14 @@ extends CharacterBody2D
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 @onready var state_machine: Node = $State
 @onready var available_states: Array = ["idle", "move", "jump", "fall"]
+@onready var health_bar: ProgressBar = $HealthBar
 
 @onready var face: String = "up"
 var health := 100
 func _ready() -> void:
 	state_machine.init(self, available_states)
 	Global.player = self
+	health_bar.init(health)
 
 # trying to avoid _unhandled_input
 func _input(event: InputEvent) -> void:
@@ -16,6 +18,12 @@ func _input(event: InputEvent) -> void:
 
 func _physics_process(delta: float) -> void:
 	state_machine.process_physics(delta)
+	#codigo pra testar a vida (b diminui, enter aumenta)
+	if Input.is_action_just_pressed("backwards"):
+		take_damage(5)
+	elif Input.is_action_just_pressed("ui_accept"):
+		take_damage(-5)
+	#fim do codigo descartavel
 
 func _process(delta: float) -> void:
 	state_machine.process_frame(delta)
@@ -47,3 +55,4 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 
 func take_damage(amount:int):
 	health -= amount
+	health_bar.set_health(health)
