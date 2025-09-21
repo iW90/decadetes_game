dbcd7fca8b1e8137bf94a23a7ec8855c59f3d146
Everton <egeraldo@student.42sp.org.br>
2025-09-20 22:56:20 -0300

    :pencil: feat: adiciona área de dano e lógica de entrada para o goblin feminino

--- scenes/characters/goblin_female.gd
+++ scenes/characters/goblin_female.gd
@@ -24,6 +24,20 @@ func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	print("viewport: ", viewport, " event: ", event, " index: ", shape_idx)
 
 
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body == self:
+		print("my own body")
+		return
+	if body is CharacterBody2D:
+		body.queue_free()
+	else:
+		print("body is ", body)
+
+
+func _on_area_2d_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	print("a body entered: ", body_rid, " body: ", body, " body_shape_index: ", body_shape_index, " local shape index: ", local_shape_index)
+
+
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	var enemy = area.get_parent()
 	if enemy is CharacterBody2D:
