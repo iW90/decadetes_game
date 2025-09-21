9d43d7b5f4ed8560a440e5eed1aa6d5c6fda2a53
Marcos <marcos.chiu@gmail.com>
2025-09-20 21:53:16 -0300

    intentional goblin fix

--- scenes/characters/goblin_female.gd
+++ scenes/characters/goblin_female.gd
@@ -2,11 +2,9 @@ extends CharacterBody2D
 
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 @onready var state_machine: Node = $State
-@onready var available_states: Array = ["idle", "move", "jump", "fall", "auto-move"]
-@onready var face: String = "up"
+@onready var available_states: Array = ["idle", "move", "jump", "fall"]
 
-@export var last_direction: String = "down"
-var health = 100
+@onready var face: String = "up"
 
 func _ready() -> void:
 	state_machine.init(self, available_states)
@@ -25,17 +23,6 @@ func _process(delta: float) -> void:
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	print("viewport: ", viewport, " event: ", event, " index: ", shape_idx)
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body == self:
-		print("my own body")
-		return
-	if body is CharacterBody2D:
-		body.queue_free()
-	else:
-		print("body is ", body)
-
-func _on_area_2d_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
-	print("a body entered: ", body_rid, " body: ", body, " body_shape_index: ", body_shape_index, " local shape index: ", local_shape_index)
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	var enemy = area.get_parent()
