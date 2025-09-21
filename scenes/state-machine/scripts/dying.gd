b2f3671e3b253150ea29e58b5d65ed25af59347d
threedp <davy.paulino@aluno.ifsp.edu.br>
2025-09-21 10:24:13 -0300

    feat: added tower actions and game-over for tower

--- scenes/state-machine/scripts/dying.gd
+++ scenes/state-machine/scripts/dying.gd
@@ -5,5 +5,9 @@ extends State
 func enter() -> void:
 	super()
 
+func _ready() -> void:
+	if parent and "_on_animation_dying_finished" in parent:
+		parent.anim.animation_finished.connect(parent._on_animation_dying_finished)
+
 func process_physics(_delta: float):
 	return null
