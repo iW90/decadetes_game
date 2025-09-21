fd41f737b5b04aebc00f838cbcd248a21b5654e0
threedp <davy.paulino@aluno.ifsp.edu.br>
2025-09-21 11:47:45 -0300

    feat: anemy attacks

--- scenes/state-machine/scripts/attack.gd
+++ scenes/state-machine/scripts/attack.gd
@@ -15,14 +15,18 @@ func process_physics(_delta: float):
 	if Global.player.health <= 0:
 		parent.state_machine.change_state_by_name("idle")
 		return null
-	
+		
 	if parent.name == "Enemy":
 		parent.coldown()
-
-
 
 	if parent.name == "GoblinFemale":
 		if parent.anim.frame == 9:
 			parent.state_machine.change_state_by_name("idle")			
 			parent.is_enable_damage_area(false)
+			
+	if parent.name == "Tower":
+		parent.state_machine.change_state_by_name("damage")
+		if parent and "_on_animation_damage_finished" in parent:
+			parent.anim.animation_finished.connect(parent._on_animation_damage_finished)
+
 	return null
