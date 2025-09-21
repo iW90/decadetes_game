d3397bf89cd25c74447a96c7df8dd90275281c2d
iW90 <ingrid.wagner.dev@gmail.com>
2025-09-21 11:47:47 -0300

    feat: add victory after 2 waves

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,3 +1,4 @@
+# Main.gd
 extends Node
 
 @export var enemy_scene: PackedScene
@@ -5,64 +6,81 @@ extends Node
 @onready var hud: CanvasLayer = $Hud
 
 var max_enemies := 5
+var max_waves := 2
 var current_wave := 1
 
 var time_between_enemies := 1
 var time_between_waves := 2
 
 var active_enemies: Array = []
+var is_spawning := false
 var enemy_dead := 0
 
-var player: Node2D = null
-var tower: Node2D = null
+var player = null
+var tower = null
+var game_over := false
 
 func _ready() -> void:
 	player = Global.player
 	tower = $Map/Tower
-	start_wave()
 	hud.set_wave(current_wave)
 	hud.set_score(enemy_dead)
-	
-func start_wave() -> void:
-	var enemy_count = max_enemies * current_wave
-	spawn_wave(enemy_count)
+	create_wave()
 
-func spawn_wave(enemy_count: int) -> void:
-	for i in range(enemy_count):
+func create_wave():
+	if is_spawning or game_over: return
+
+	var count_enemies = max_enemies * current_wave
+	for i in range(count_enemies):
+		if game_over: return
 		spawn_enemy()
 		await get_tree().create_timer(time_between_enemies).timeout
 
-func next_wave() -> void:
+func next_wave():
+	if game_over: return
 	await get_tree().create_timer(time_between_waves).timeout
+	if game_over: return
 	current_wave += 1
 	hud.set_wave(current_wave)
-	start_wave()
-	
-func spawn_enemy() -> void:
+	is_spawning = false
+	create_wave()
+
+func spawn_enemy():
+	if game_over: return
+	is_spawning = true
 	var enemy = enemy_scene.instantiate()
 	add_child(enemy)
 	enemy.position = calculate_spawn_pos()
 	enemy.move_to_position(tower.global_position)
-	
+
 	active_enemies.append(enemy)
+	enemy.tree_exited.connect(on_enemy_exite.bind(enemy))
 
-	enemy.defeated.connect(on_enemy_exit.bind(enemy))
-#	enemy.defeated.connect(tower._on_enemy_defeated) <- POE A FUNCAO AQUI
-
-func on_enemy_exit(enemy: Node2D):
+func on_enemy_exite(enemy):
+	if game_over: return
 	if enemy in active_enemies:
 		active_enemies.erase(enemy)
 		enemy_dead += 1
 		hud.set_score(enemy_dead)
-	
+
 	if active_enemies.is_empty():
-		call_deferred("next_wave")
+		if current_wave >= max_waves:
+			end_game_and_show_win()
+		else:
+			next_wave()
 
 func calculate_spawn_pos() -> Vector2:
+	if game_over: return Vector2.ZERO
 	var screen_size = get_viewport().get_visible_rect().size
 	var player_pos = player.global_position
 
 	var spawn_distance = screen_size.length() / 2 + spawn_margin
 	var angle = randf_range(0, TAU)
-
 	return player_pos + Vector2.RIGHT.rotated(angle) * spawn_distance
+
+func end_game():
+	game_over = true
+
+func end_game_and_show_win():
+	end_game()
+	get_tree().change_scene_to_file("res://scenes/Win.tscn")
